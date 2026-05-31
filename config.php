a5c81c7a1a436114c3ef7eadc4e30ebcd38dd60e
Henry Franco <hfrancoe@miumg.edu.gt>
2026-05-30 18:24:33 -0600

    Actualizar conexion para Azure

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost';
-$db   = 'transmetro_db';
-$user = 'root';
-$pass = '';
+$host = getenv('DB_HOST') ?: 'localhost';
+$db   = getenv('DB_NAME') ?: 'transmetro_db';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -29,7 +30,9 @@ function verificarSesion() {
 
 function tieneRol($roles) {
     verificarSesion();
-    if (is_string($roles)) { $roles = [$roles]; }
+    if (is_string($roles)) { 
+        $roles = [$roles]; 
+    }
     return in_array($_SESSION['rol'] ?? '', $roles, true);
 }
 
